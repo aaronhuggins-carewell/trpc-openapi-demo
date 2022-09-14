8bbafe07c1e02b8284360248bad35f877a1f8dea
Aaron Huggins <aaron.huggins@carewell.com>
2022-09-14 14:38:41 -0500

    A few more changes

--- apps/mycompany-api-routes/src/createRouter.ts
+++ apps/mycompany-api-routes/src/createRouter.ts
@@ -2,5 +2,5 @@ import * as trpc from '@trpc/server';
 import { OpenApiMeta } from 'trpc-openapi';
 
 export default function createRouter() {
-	return trpc.router<{}, OpenApiMeta>();
+	return trpc.router<Record<string, unknown>, OpenApiMeta>();
 }
